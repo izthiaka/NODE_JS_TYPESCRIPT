7a2c077ac7ef94da71856b5ab47f85e12b8d9155
Ismaila T. Badji <78214503+izthiaka@users.noreply.github.com>
2023-03-11 13:37:00 +0000

    config: nodejs config

--- app.ts
+++ app.ts
@@ -2,11 +2,16 @@ import dotenv from "dotenv"
 
 import app from "./routes/app_route_v1.js"
 
+import { DbAdapter } from "./services/database/mongo/DbAdapter.js"
+
 dotenv.config()
 
 const PORT = process.env.APP_PORT || 8080
 const URL = process.env.APP_URL
 const { APP_NAME } = process.env
+
+const database = new DbAdapter()
+database.connectDatabase()
 
 app.use((req, res) => {
     res.status(404)
--- server.ts
+++ server.ts
@@ -4,6 +4,6 @@ const PORT = process.env.APP_PORT || 5000
 
 const { log } = console
 
-app.listen(PORT, async(res) => {
+app.listen(PORT, async () => {
     log(`listening on PORT ${PORT}`)
 })
